00fc4c9394daa42f9dd568b8c8e472d07d9c3079
RuwanaraT <sewminiruwanara@gmail.com>
2021-09-24 18:03:16 +0530

    Update DBManager.java

--- app/src/main/java/com/example/doctorchannelingapp/DBManager.java
+++ app/src/main/java/com/example/doctorchannelingapp/DBManager.java
@@ -5,12 +5,17 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+
 public class DBManager extends SQLiteOpenHelper {
 
-    private static final int VERSION = 1;
+    private static final int VERSION = 2;
     private static final String DATABASE = "amWell";
     private static final String TABLE_NAME = "patient";
     private static final String TABLE_NAME_1 = "channel";
@@ -41,6 +46,21 @@ public class DBManager extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        // channel table create query
+        String TABLE_CHANNEL_QUERY = "CREATE TABLE "+TABLE_NAME_1+" " +
+                "("
+                +PID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +P_NAME + " TEXT,"
+                +P_NUMBER + " TEXT,"
+                +P_NIC + " TEXT,"
+                +P_EMAIL+ " TEXT,"
+                +DOCTOR + " TEXT,"
+                +SPECIALIZATION+" TEXT" +
+                ");";
+
+        // run the create query
+        db.execSQL(TABLE_CHANNEL_QUERY);
+
         // patient table create query
         String TABLE_PATIENT_QUERY = "CREATE TABLE "+TABLE_NAME+" " +
                 "("
@@ -58,35 +78,22 @@ public class DBManager extends SQLiteOpenHelper {
         // run the create query
         db.execSQL(TABLE_PATIENT_QUERY);
 
-        // channel table create query
-        String TABLE_CHANNEL_QUERY = "CREATE TABLE "+TABLE_NAME_1+" " +
-                "("
-                +PID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
-                +P_NAME + " TEXT,"
-                +P_NUMBER + " TEXT,"
-                +P_NIC + " TEXT,"
-                +P_EMAIL+ " TEXT,"
-                +DOCTOR + " TEXT,"
-                +SPECIALIZATION+" TEXT" +
-                ");";
-
-        // run the create query
-        db.execSQL(TABLE_CHANNEL_QUERY);
     }
 
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        // drop table query for channel table
+        String DROP_TABLE_QUERY_1 = "DROP TABLE IF EXISTS " + TABLE_NAME_1;
+        //drop older table if existed
+        db.execSQL(DROP_TABLE_QUERY_1);
+
         // drop table query for patient table
         String DROP_TABLE_QUERY = "DROP TABLE IF EXISTS " + TABLE_NAME;
         //drop older table if existed
         db.execSQL(DROP_TABLE_QUERY);
 
-        // drop table query for channel table
-        String DROP_TABLE_QUERY_1 = "DROP TABLE IF EXISTS " + TABLE_NAME_1;
-        //drop older table if existed
-        db.execSQL(DROP_TABLE_QUERY_1);
 
         // create tables again
         onCreate(db);
@@ -153,7 +160,7 @@ public class DBManager extends SQLiteOpenHelper {
     }
 
     // method for inset channel
-    public Boolean addChannel(Channel channel) {
+    public void addChannel(Channel channel) {
 
         SQLiteDatabase sqLiteDatabase = getWritableDatabase();
 
@@ -166,16 +173,95 @@ public class DBManager extends SQLiteOpenHelper {
         contentValues.put(DOCTOR, channel.getdName());
         contentValues.put(SPECIALIZATION, channel.getSep());
 
-        long result = sqLiteDatabase.insert(TABLE_NAME_1, null, contentValues);
+        sqLiteDatabase.insert(TABLE_NAME_1, null, contentValues);
 
-        if(result == -1) {
+        sqLiteDatabase.close();
 
-            return false;
-        }
-        else {
-
-            return true;
-        }
     }
 
+    // method for delete channel
+    public void deleteChannel(String refNo) {
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.delete(TABLE_NAME_1, "p_nic = ?", new String[] {refNo});
+        db.close();
+    }
+
+    // get all channels
+    public List<Channel> getAllToDos(){
+
+        List<Channel> channels = new ArrayList();
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "SELECT * FROM "+TABLE_NAME_1;
+
+        Cursor cursor = db.rawQuery(query,null);
+
+        if(cursor.moveToFirst()){
+            do {
+                // Create new channel object
+                Channel channel = new Channel();
+
+                channel.setId(cursor.getInt(0));
+                channel.setpName(cursor.getString(1));
+                channel.setpPhone(cursor.getString(2));
+                channel.setpNIC(cursor.getString(3));
+                channel.setdName(cursor.getString(4));
+                channel.setSep(cursor.getString(4));
+
+                channels.add(channel);
+            }while (cursor.moveToNext());
+        }
+        return channels;
+    }
+
+    // get a single channel
+    public Channel getSingleChannel(String id){
+        SQLiteDatabase db = getWritableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME_1,new String[]{PID,P_NAME,P_NUMBER,P_NIC,P_EMAIL,DOCTOR,SPECIALIZATION},
+                P_NIC + "= ?",new String[]{id}
+                ,null,null,null);
+
+        Channel channel;
+        if(cursor != null){
+            cursor.moveToFirst();
+            channel = new Channel(
+                    cursor.getInt(0),
+                    cursor.getString(1),
+                    cursor.getString(2),
+                    cursor.getString(3),
+                    cursor.getString(4),
+                    cursor.getString(5),
+                    cursor.getString(6)
+            );
+            return channel;
+        }
+        return null;
+    }
+
+    // method for update single channel
+    public boolean updateSingleChannel(Channel channel){
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(P_NAME,channel.getpName());
+        contentValues.put(P_NUMBER, channel.getpPhone());
+        contentValues.put(P_NIC, channel.getpNIC());
+        contentValues.put(P_EMAIL, channel.getpEmail());
+        contentValues.put(P_EMAIL, channel.getpEmail());
+        contentValues.put(DOCTOR, channel.getdName());
+        contentValues.put(SPECIALIZATION, channel.getSep());
+
+        int status = db.update(TABLE_NAME_1,contentValues,PID +" =?",
+                new String[]{String.valueOf(channel.getId())});
+
+        if(status > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
