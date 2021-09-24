68e18ebbc76071401cd6747533664d7301419556
RuwanaraT <sewminiruwanara@gmail.com>
2021-09-24 18:02:53 +0530

    Update ChannelingReceipt.java

--- app/src/main/java/com/example/doctorchannelingapp/ChannelingReceipt.java
+++ app/src/main/java/com/example/doctorchannelingapp/ChannelingReceipt.java
@@ -1,16 +1,27 @@
 package com.example.doctorchannelingapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class ChannelingReceipt extends AppCompatActivity {
 
     private TextView tv_refNo;
     private TextView tv_doctor;
     private TextView tv_special;
+    private Button btn_update;
+    Context context;
+    DBManager dbManager;
 
     String RefNo;
     String Doctor;
@@ -24,6 +35,9 @@ public class ChannelingReceipt extends AppCompatActivity {
         tv_refNo = findViewById(R.id.tv_refno);
         tv_doctor = findViewById(R.id.tv_doctor);
         tv_special = findViewById(R.id.tv_special);
+        btn_update = findViewById(R.id.btn_update);
+        context = this;
+        dbManager = new DBManager(context);
 
         Intent intent = getIntent();
 
@@ -36,5 +50,45 @@ public class ChannelingReceipt extends AppCompatActivity {
         tv_refNo.setText(RefNo);
         tv_doctor.setText(Doctor);
         tv_special.setText(Special);
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Test");
+                builder.setMessage("Message");
+
+                builder.setPositiveButton("Finished", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        startActivity(new Intent(context,PatientDashboard.class));
+                    }
+                });
+
+                builder.setNeutralButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dbManager.deleteChannel(RefNo);
+                        startActivity(new Intent(context,PatientDashboard.class));
+                    }
+                });
+
+                builder.setNegativeButton("Update", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Intent intent1 = new Intent(context, UpdateChannelingForm.class);
+                        intent1.putExtra("itm", RefNo);
+                        startActivity(intent1);
+
+
+                    }
+                });
+
+                builder.show();
+            }
+        });
     }
 }
